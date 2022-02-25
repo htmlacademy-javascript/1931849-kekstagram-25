f8697093ec406e32e6d92b00517df7c31b70d57a
ruslan.aisin <ruslan-spb@mail.ru>
2022-02-25 11:40:53 +0300

    bug fix 2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function randomNumber(value1, value2) {
   return Math.floor(Math.random() * (value1 + value2) + value1);
 }
 // Print random value on page
-document.write('Случайное число: ', randomNumber(0, 100), '\n');
+document.write('Случайное число: ', randomNumber(0, 100));
 
 function checkLegth(string, maxLength) {
   return string.length <= maxLength;
